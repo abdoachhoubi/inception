65dcb5d434426017f5dc4b3c311196d88b77f956
abdoachhoubi <aachhoub@gmail.com>
2023-09-03 11:59:37 -0400

    FInished :')

--- srcs/requirements/mariadb/script/config.sh
+++ srcs/requirements/mariadb/script/config.sh
@@ -7,13 +7,17 @@ service mysql start
 sleep 3
 
 # Create a database and a user with full privileges.
-mysql -u root -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_USER_PASSWORD';"
+mysql -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_USER_PASSWORD';"
 
-mysql -u root -e "CREATE DATABASE IF NOT EXISTS $WP_DATABASE;"
+mysql -e "CREATE DATABASE IF NOT EXISTS $WP_DATABASE;"
 
-mysql -u root -e "GRANT ALL PRIVILEGES ON $WP_DATABASE.* to '$MYSQL_USER'@'%';"
+mysql -e "GRANT ALL PRIVILEGES ON $WP_DATABASE.* to '$MYSQL_USER'@'%';"
 
-mysql -u root -e "FLUSH PRIVILEGES;"
+mysql -e "FLUSH PRIVILEGES;"
+
+# Create root user.
+mysql -e "ALTER USER 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
+
 
 # Stop the mysql daemon.
 service mysql stop
