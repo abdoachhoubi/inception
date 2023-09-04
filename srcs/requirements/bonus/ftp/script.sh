3e94141f51c508efaf72be4236a5fdbdd3a946a2
abdoachhoubi <aachhoub@gmail.com>
2023-09-04 13:20:42 -0400

    FInished everything

--- srcs/requirements/bonus/ftp/script.sh
+++ srcs/requirements/bonus/ftp/script.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Define the username
-username="aachhoub"
+username=$FTP_USER
 
 # Check if the user already exists
 if id "$username" &>/dev/null; then
     echo "User $username already exists."
 else
     # If the user does not exist, create it
-    useradd -m -p $(openssl passwd -1 aachhoub) "$username"
+    useradd -m -p $(openssl passwd -1 $FTP_PASSWORD) "$username"
     echo "User $username has been created."
 fi
 
--- srcs/requirements/mariadb/script/config.sh
+++ srcs/requirements/mariadb/script/config.sh
@@ -1,26 +1,13 @@
 #!/bin/bash
 
-# Start the mysql daemon in the background.
-service mysql start
+service mariadb start
 
-# Because we need to wait for the mysql daemon to start before running the rest of the script.
-sleep 3
+sleep 5
 
-# Create a database and a user with full privileges.
-mysql -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_USER_PASSWORD';"
+mariadb -e	"CREATE DATABASE IF NOT EXISTS $WP_DATABASE"
+mariadb -e	"CREATE USER  IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_USER_PASSWORD'"
+mariadb -e	"GRANT ALL PRIVILEGES ON * . * TO '$MYSQL_USER'@'%';"
+mariadb -e	"FLUSH PRIVILEGES;"
+# mariadb -e	"ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
 
-mysql -e "CREATE DATABASE IF NOT EXISTS $WP_DATABASE;"
-
-mysql -e "GRANT ALL PRIVILEGES ON $WP_DATABASE.* to '$MYSQL_USER'@'%';"
-
-mysql -e "FLUSH PRIVILEGES;"
-
-# Create root user.
-mysql -e "ALTER USER 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
-
-
-# Stop the mysql daemon.
-service mysql stop
-
-# Start the mysql daemon in the foreground.
-mysqld_safe
+mariadbd
